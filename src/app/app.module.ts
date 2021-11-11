b250073015586fe015960a10188f8b37fb8db15a
deontay jefferson <djefferson46@gmail.com>
2021-11-11 05:33:48 -0800

    New Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//import { HttpModule } from '@angular/http';
+//import { FromsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //FromsModule,
+   // HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+//building and exporting a component
+@Component({//this is a component decorator
+ selector: "app-server", //this is the component tag <app-server>
+ templateUrl: "./server.component.html"
+}) 
+export class ServerComponent {
+
+}
